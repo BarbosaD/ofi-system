29131dc1f164ff6ae990a9031122038c30614ecf
joaopella <jvcpella12@gmail.com>
2023-05-30 09:31:16 -0300

    teste

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
             <div class="header">
                 <div id="info">
                     <img id="avatar" src="./imgs/avatar.png"/>
-                    <span id="user"> Kaidscdsros </span>
+                    <span id="user"> Kairos </span>
                     <img id="arrow" src="./imgs/down-arrow.png"/>
                 </div>
                 <div id="dropdown">
